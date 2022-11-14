a41d38d7fc4239c4189fdfe41b4972272a4933dd
Sagie Bar Hama <58434974+Sagie-BH@users.noreply.github.com>
2022-11-14 20:51:45 +0200

    Lesson Context - Custom Hook

--- my-use-context/src/AuthContext/MyContext.jsx
+++ my-use-context/src/AuthContext/MyContext.jsx
@@ -1,23 +1,17 @@
-import React, { useState } from "react"
+import React from "react"
 import { useEffect } from "react";
 import Auth from "./Auth";
-import AuthContext from "./AuthContext";
 import ColorContext from "./ColorContext";
+import { MyAuthContext } from "./MyAuthContext";
+
 const MyContext = () => {
-    const [authStatus, setAuthStatus] = useState(false);
-
-    const login = () => {
-      // Validation process
-        setAuthStatus(true);
-    }
-
     return (
-      <AuthContext.Provider value={{ status: authStatus, login: login }}>
+      <MyAuthContext>
         <MiddleWare />
         <ColorContext.Provider>
           <MiddleWare />
         </ColorContext.Provider>
-      </AuthContext.Provider>
+      </MyAuthContext>
     );
   }
   
@@ -29,6 +23,5 @@ const MyContext = () => {
         <div>
             <Auth/>
         </div>
-
       );
   }

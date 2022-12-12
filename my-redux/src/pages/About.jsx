3201063e64c50040c4e3ad53a7a8fbb1f361c102
Sagie Bar Hama <58434974+Sagie-BH@users.noreply.github.com>
2022-12-12 19:45:22 +0200

    .

---
+++ my-redux/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>This Is About Page</div>
+  )
+}
+
+export default About
---
+++ my-redux/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>This Is Home Page</div>
+  )
+}
+
+export default Home
